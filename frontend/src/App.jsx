71ff848fca85aa8dab972061bdec5e27c4afc3e3
Prem sahani <131917799+prembu@users.noreply.github.com>
2025-01-25 13:55:55 +0530

    New Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
